c0545c3c613939326698996d9e54ecd362c304d1
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-12-23 19:09:36 +0530

    Add a dummy API endpoint to get all the contacts

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.send('<h1>~ Hello World! ~</h1>');
 });
 
+app.get('/api/contacts', (req, res) => {
+    res.status(200)
+        .json({message: 'Returns a list of all the contacts'});
+});
+
 app.listen(port, () => {
     console.log(`My Contacts backend app listening on port ${port}`);
 });
