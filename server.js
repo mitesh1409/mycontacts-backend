2f2060aeb205330a7ca44abfdc1e4b9b4e71e2c0
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-12-25 18:14:34 +0530

    Delete birds router module

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
-const birdsRouter = require('./routes/birds');
 const contactsRouter = require('./routes/contacts');
 
-app.use('/birds', birdsRouter);
 app.use('/contacts', contactsRouter);
 
 app.get('/', (req, res) => {
