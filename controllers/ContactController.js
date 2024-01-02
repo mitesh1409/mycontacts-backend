3f6194c11d51b4b47b110eecf797e11cc1696e0a
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2024-01-02 10:34:02 +0530

    Install and use express-async-handler

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,17 +1,19 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc GET /contacts - Get all the contacts
 // @route GET /contacts
 // @access public
-const getContacts = async (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
     res.status(200)
         .json({
             message: 'GET /contacts - Get all the contacts'
         });
-};
+});
 
 // @desc POST /contacts - Create a contact
 // @route POST /contacts
 // @access public
-const createContact = async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     const { name, email, phone } = req.body;
 
     if (!name || !email || !phone) {
@@ -23,37 +25,37 @@ const createContact = async (req, res) => {
         .json({
             message: 'POST /contacts - Create a contact'
         });
-};
+});
 
 // @desc GET /contacts/:id - Get contact with id
 // @route GET /contacts/:id
 // @access public
-const getContact = async (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200)
         .json({
             message: `GET /contacts/:id - Get contact with id: ${req.params.id}`
         });
-};
+});
 
 // @desc PUT /contacts/:id - Update contact with id
 // @route PUT /contacts/:id
 // @access public
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200)
         .json({
             message: `PUT /contacts/:id - Update contact with id: ${req.params.id}`
         });
-};
+});
 
 // @desc DELETE /contacts/:id - Delete contact with id
 // @route DELETE /contacts/:id
 // @access public
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200)
         .json({
             message: `DELETE /contacts/:id - Delete contact with id: ${req.params.id}`
         });
-};
+});
 
 module.exports = {
     getContacts,
