265ad22aa4bffe4f5a74975944a7d5cf5410ff47
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2024-01-04 22:59:49 +0530

    Remove an unwanted variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const errorHandler = require('./middlewares/ErrorHandler');
 const app = express();
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 require('./database/connection');
 const contactsRouter = require('./routes/contacts');
